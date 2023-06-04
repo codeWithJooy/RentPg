b9e655c7f4a9c901aaba7081d1f64b8a170053af
Abhishek Hazra <enigma.abhi.ty@gmail.com>
2023-06-04 06:25:14 +0530

    Header Component Added

---
+++ frontend/src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="header">
+      <div className="logo"></div>
+      <div className="headerTitle"></div>
+      <div className="headerBell"></div>
+    </div>
+  );
+};
