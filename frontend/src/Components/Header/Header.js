94312196420fa41bae4512c1431a648fbfcba587
Abhishek Hazra <enigma.abhi.ty@gmail.com>
2023-06-04 07:37:37 +0530

    header Done

--- frontend/src/Components/Header/Header.js
+++ frontend/src/Components/Header/Header.js
@@ -1,16 +1,22 @@
 import React from "react";
 import "./Header.css";
 
-const Home = () => {
+const Header = () => {
   return (
     <div className="header">
       <div className="headerContainer">
         <div className="logo">
           <img src="Assets/Header/RentPG.jpg" />
         </div>
+        <div className="headerTitle">
+          <p>AbhiPG</p>
+        </div>
+        <div className="headerNotifications">
+          <img src="Assets/Header/notification.png" />
+        </div>
       </div>
     </div>
   );
 };
 
-export default Home;
+export default Header;
