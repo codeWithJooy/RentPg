16a38670812eb4f27d45019464dfca69169787df
Abhishek Hazra <enigma.abhi.ty@gmail.com>
2023-06-05 17:46:55 +0530

    Hightligts added in main Page

--- frontend/src/Pages/Home/Home.js
+++ frontend/src/Pages/Home/Home.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
+import Highlights from "../../Components/Home/Highlights/Highlights";
+
 const Home = () => {
   return (
     <div className="home">
       <Header />
+      <div className="homeContainer">
+        <Highlights />
+      </div>
       <Footer />
     </div>
   );
