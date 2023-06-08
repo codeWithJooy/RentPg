13d006383b8a66fe962fbe37b00128e42e04175f
Abhishek Hazra <enigma.abhi.ty@gmail.com>
2023-06-08 20:39:31 +0530

    New Change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,14 @@ import "./App.css";
 
 import Home from "./Pages/Home/Home";
 import Property from "./Pages/Property/Propert";
-
+import AddUnit from "./Pages/Property/AddUnit/AddUnit";
 const App = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/home" component={Home} />
         <Route path="/property" component={Property} />
+        <Route path="/property/add/unit" component={AddUnit} />
       </Switch>
     </BrowserRouter>
   );
---
+++ frontend/src/Pages/Property/AddUnit/AddUnit.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "";
+const AddUnit = () => {
+  return <div></div>;
+};
+
+export default AddUnit;
