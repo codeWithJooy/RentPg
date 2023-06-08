e0c2cf8972ee67a8edcf4206813fdccc19762a0c
Abhishek Hazra <enigma.abhi.ty@gmail.com>
2023-06-08 20:57:45 +0530

    splash sreen and Signup image

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./App.css";
 
+import Splash from "./Pages/Splash/Splash";
 import Home from "./Pages/Home/Home";
 import Property from "./Pages/Property/Propert";
 import AddUnit from "./Pages/Property/AddUnit/AddUnit";
@@ -8,6 +9,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path="/splash" component={Splash} />
         <Route path="/home" component={Home} />
         <Route path="/property" component={Property} />
         <Route path="/property/add/unit" component={AddUnit} />
--- frontend/src/Pages/Property/AddUnit/AddUnit.js
+++ frontend/src/Pages/Property/AddUnit/AddUnit.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "";
 const AddUnit = () => {
   return <div></div>;
 };
