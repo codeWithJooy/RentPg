2b593dd169bdac65c01bfb927f1d6c74d991e5ad
Abhishek Hazra <enigma.abhi.ty@gmail.com>
2023-06-09 07:40:29 +0530

    Getting Started Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./App.css";
 
 import Splash from "./Pages/Splash/Splash";
+import GetStarted from "./Pages/GetStarted/GetStarted";
 import Home from "./Pages/Home/Home";
 import Property from "./Pages/Property/Propert";
 import AddUnit from "./Pages/Property/AddUnit/AddUnit";
@@ -10,6 +11,7 @@ const App = () => {
     <BrowserRouter>
       <Switch>
         <Route path="/splash" component={Splash} />
+        <Route path="/get-started" component={GetStarted} />
         <Route path="/home" component={Home} />
         <Route path="/property" component={Property} />
         <Route path="/property/add/unit" component={AddUnit} />
